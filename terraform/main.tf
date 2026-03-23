87a63fc814743172d7ab631370f51b40f584391f
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 17:06:28 +0600

    commit6

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,9 +36,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
