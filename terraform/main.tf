828b20c498c6b649dbd7cd39083a2ad45df0c351
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 21:44:03 +0600

    commit29

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.16.0"
+      version = ">= 4.57.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.21.0"
+      version = ">= 2.10.0"
     }
   }
 }
