031d46946b9ef9c0c75b35eb8cbb897402206668
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 21:14:26 +0600

    commit21

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,14 +21,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
