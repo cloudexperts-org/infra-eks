d0b78e8600406767523bb8dd93322bb37ee5e5d1
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 00:45:35 +0600

    commit117

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,7 +6,11 @@ module "eks" {
   cluster_version = "1.35"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnet_ids
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_private_access       = true
+  cluster_endpoint_public_access        = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   eks_managed_node_groups = {
     default = {
