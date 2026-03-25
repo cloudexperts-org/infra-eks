b0922ba0ccabe87c923d12de05e2c72e142a3eb0
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 11:32:43 +0600

    commit134

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ module "eks" {
   cluster_name = var.cluster_name
   subnet_ids   = module.vpc.public_subnet_ids
   vpc_id       = module.vpc.vpc_id
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 }
 
 module "iam" {
