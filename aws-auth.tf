1c7a611fd61ade07cc20dff7924098990d46f66a
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 18:01:37 +0600

    commit11

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name   # <--- use cluster_name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name   # <--- use cluster_name
 }
 
 provider "kubernetes" {
