2ee9bdf3116bbc4b5f30622a5e563850cdaf0642
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 18:24:00 +0600

    commit17

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -10,7 +10,3 @@ output "cluster_arn" {
 output "eks_managed_node_group_names" {
   value = module.this_eks.eks_managed_node_groups
 }
-
-output "eks_managed_node_group_arns" {
-  value = module.this_eks.eks_managed_node_groups
-}
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
 }
+
+output "eks_managed_node_group_iam_roles" {
+  value = { for k, v in module.eks.managed_node_groups : k => v.iam_role_arn }
+}
