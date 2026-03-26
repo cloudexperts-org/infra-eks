2b01d270d6df3b4cbdf46b7341f7d3eaa4fecafb
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 02:23:31 +0600

    commit3

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,9 +1,15 @@
 terraform {
   required_version = "~> 1.5.7"
 
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.22.0" }
+    helm = { source = "hashicorp/helm", version = "~> 2.16" }
+  }
+
   backend "s3" {
     bucket         = "cloudex-terraform-state-bucket"
-    key            = "eks/dev/terraform.tfstate"  # temporarily hardcoded
+    key            = "eks/dev/terraform.tfstate"  # can make dynamic later
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
--- infra/envs/dev/provider.tf
+++ infra/envs/dev/provider.tf
@@ -2,21 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  required_version = "~> 1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.22.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.16"
-    }
-  }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
