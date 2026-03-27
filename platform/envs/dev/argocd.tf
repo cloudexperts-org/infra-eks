a87033cc25bd87dc0da7cca8cfb3c0959a670c64
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 17:57:19 +0600

    commit43

--- platform/envs/dev/argocd.tf
+++ platform/envs/dev/argocd.tf
@@ -23,13 +23,15 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 # Kubernetes provider (aliased)
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
+
+# Helm provider (default, but will use the kubernetes provider via resource)
+provider "helm" {}
 
 # Helm release
 resource "helm_release" "argocd" {
