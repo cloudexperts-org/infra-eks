8ef5048e79619fb8f4a9953468b8f61781e5771d
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 14:33:44 +0600

    commit28

--- platform/envs/dev/argocd.tf
+++ platform/envs/dev/argocd.tf
@@ -1,10 +1,11 @@
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
 # EKS cluster data
 data "aws_eks_cluster" "eks" {
-  name = "dev-eks-cluster"
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 data "aws_eks_cluster_auth" "eks" {
@@ -13,22 +14,19 @@ data "aws_eks_cluster_auth" "eks" {
 
 # Kubernetes provider
 provider "kubernetes" {
-  alias                  = "eks"
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-provider "helm" {
-  # no nested kubernetes block needed
-}
+# Helm provider (will use the default Kubernetes provider)
+provider "helm" {}
 
-# Helm release
+# Helm release for ArgoCD
 resource "helm_release" "argocd" {
   name             = "argocd"
   namespace        = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   create_namespace = true
-
 }
