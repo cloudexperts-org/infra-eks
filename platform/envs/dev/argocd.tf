589f2f0ce9529b78ca4e2610aa6bc4151a8848ba
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 19:07:35 +0600

    commit54

--- platform/envs/dev/argocd.tf
+++ platform/envs/dev/argocd.tf
@@ -15,18 +15,20 @@ provider "aws" {
 
 # Get cluster info from EKS
 data "aws_eks_cluster" "eks" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "eks" {
-  name = data.aws_eks_cluster.eks.outputs.cluster_name
+  name = data.aws_eks_cluster.eks.name
 }
 
 # Kubernetes provider (used by Helm)
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
 }
 
 # Helm provider uses Kubernetes provider
